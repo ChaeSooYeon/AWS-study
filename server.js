aa1e1130aa74d7019d45585cc318e2e31ea46bc6
csy-seoulauction <csy@seoulauction.com>
2023-11-15 11:38:09 +0900

    서버설정변경 - index.html 노출 성공

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 import { createServer } from "http";
 import express from "express";
 import { join } from "path";
+import process from "process";
 
 const app = express();
 
 const port = 3000;
+const __dirname = process.cwd();
 
 //test
-app.get("/test", (req, res) => {
-    res.send("hello world");
+app.get("/ping", (req, res) => {
+    res.send("pong");
 });
 
-//app.use(express.static(join("__dirname", "dist")));
+app.use(express.static(join(__dirname, "dist")));
 
-app.use("*", (req, res) => {
-    return handle(req, res, req.url);
+app.get("*", (req, res) => {
+    res.sendFile(join("__dirname", "dist", "index.html"));
 });
 
 createServer(app).listen(port, () => {
-    console.log(`app listening at ${port}`);
+    console.log(`app listening at "http://localhost:${port}"`);
 });
